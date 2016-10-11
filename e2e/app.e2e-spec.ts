b9ece115b7829078121c2073232570cee867db21
sandeeprana <sandeep@loktra.com>
2016-10-11 15:04:03 +0530

    Initial Commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularCliSurveyamPage } from './app.po';
+
+describe('angular-cli-surveyam App', function() {
+  let page: AngularCliSurveyamPage;
+
+  beforeEach(() => {
+    page = new AngularCliSurveyamPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
